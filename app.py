20c46e4e752717d23a9b8b2b714864093040d8ce
maxmaner2 <34439331+MAXMANER@users.noreply.github.com>
2024-07-21 20:33:46 +0800

    update app.py
    read url variable from environment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 from linebot import LineBotApi
 from linebot.exceptions import LineBotApiError
 app = Flask(__name__)
